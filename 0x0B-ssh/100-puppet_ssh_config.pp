32a326e6c0cb1b3aed2476ef1abb57b127d10718
Diekkan <3950@holbertonschool.com>
2022-04-19 07:57:50 -0700

    Puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,8 +1,8 @@
 # Creates a file in /tmp.
 file { '/etc/ssh/ssh_config':
-ensure => 'present',
-owner  => 'root',
-group  => 'root',
-mode   => '0600',
-source => '/home/holberton-system_engineering-devops/0x0B-ssh/2-ssh_config',
+ensure  => 'present',
+owner   => 'root',
+group   => 'root',
+mode    => '0644',
+content => "Host *\n   PasswordAuthentication no\n   IdentityFile ~/.ssh/school",
 }
