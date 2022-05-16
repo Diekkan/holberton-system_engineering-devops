b4ef864496e1364ae960da9422c98ea4ce44582d
Diekkan <merentiel.diego@gmail.com>
2022-05-16 17:25:28 -0300

    0 fixes

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,30 +2,31 @@
 """
 Gather data from an API
 """
-from sys import argv
 import requests
+import sys
 
-try:
-    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
-    users = requests.get('https://jsonplaceholder.typicode.com/users')
-    users = users.json()
-    todos = todos.json()
-    tasks = 0
-    done = 0
-    tasktitle = """"""
-    for user in users:
-        if user.get('id') == int(argv[1]):
-            username = user.get('name')
-            break
-    for todo in todos:
-        if todo.get('userId') == int(argv[1]):
-            tasks += 1
-            tasktitle += '\t' + todo.get('title') + " \n"
-            if todo.get('completed') is True:
-                done += 1
-    txtformat = f"Employee {username} is done with tasks"
-    txtformat += f"({done}/{tasks}):\n"
-    txtformat += tasktitle
-    print(txtformat)
-except Exception:
-    pass
+if __name__ == "__main__":
+    try:
+        todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+        users = requests.get('https://jsonplaceholder.typicode.com/users')
+        users = users.json()
+        todos = todos.json()
+        tasks = 0
+        done = 0
+        tasktitle = """"""
+        for user in users:
+            if user.get('id') == int(sys.argv[1]):
+                username = user.get('name')
+                break
+        for todo in todos:
+            if todo.get('userId') == int(sys.argv[1]):
+                tasks += 1
+                if todo.get('completed') is True:
+                    done += 1
+                    tasktitle += '\t' + todo.get('title') + " \n"
+        txtformat = f"Employee {username} is done with tasks"
+        txtformat += f"({done}/{tasks}):\n"
+        txtformat += tasktitle
+        sys.stdout.write(txtformat)
+    except Exception:
+        pass
